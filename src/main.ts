d1369d260debc8b2ae568b8aafb5aead581a6c44
nokotan <kamenokonokotan@gmail.com>
2022-08-14 15:06:38 +0900

    update siv3d-playground to 0.0.14

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { downloadAndUnzipExtensions, downloadAndUnzipVSCode } from "./download";
 
 async function main() {
     await downloadAndUnzipVSCode("stable");
-    await downloadAndUnzipExtensions("siv3d-playground", "https://github.com/nokotan/siv3d-web-playground/releases/download/v0.0.13/siv3d-playground-npm-package");
+    await downloadAndUnzipExtensions("siv3d-playground", "https://github.com/nokotan/siv3d-web-playground/releases/download/v0.0.14/siv3d-playground.tgz");
     await downloadAndUnzipExtensions("emscripten-remote-build", "https://github.com/nokotan/emscripten-remote-build/releases/download/v0.0.2/emscripten-remote-build-npm-package");
     fs.copyFileSync("index.html", "vscode/vscode-web/index.html");
 }
