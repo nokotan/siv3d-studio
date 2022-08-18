33383d137295dd523f29dc851d988718620b0d57
nokotan <nokotan@users.noreply.github.com>
2022-08-18 09:48:29 +0000

    deploy: 284d6c698daf39838cc46038b505515161e6beff

--- out/vs/base/common/worker/simpleWorker.nls.js
+++ out/vs/base/common/worker/simpleWorker.nls.js
@@ -2,4 +2,4 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/define("vs/base/common/worker/simpleWorker.nls",{"vs/base/common/platform":["_"]});
 
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/6d9b74a70ca9c7733b29f0456fd8195364076dda/core/vs/base/common/worker/simpleWorker.nls.js.map
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/e4503b30fc78200f846c62cf8091b76ff5547662/core/vs/base/common/worker/simpleWorker.nls.js.map
--- out/vs/editor/common/services/editorSimpleWorker.nls.js
+++ out/vs/editor/common/services/editorSimpleWorker.nls.js
@@ -2,4 +2,4 @@
  * Copyright (C) Microsoft Corporation. All rights reserved.
  *--------------------------------------------------------*/define("vs/editor/common/services/editorSimpleWorker.nls",{"vs/base/common/platform":["_"]});
 
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/6d9b74a70ca9c7733b29f0456fd8195364076dda/core/vs/editor/common/services/editorSimpleWorker.nls.js.map
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/e4503b30fc78200f846c62cf8091b76ff5547662/core/vs/editor/common/services/editorSimpleWorker.nls.js.map
