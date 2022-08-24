902fdb5d4842c68b8acd3dc49d6a8a6e96ec7ed1
nokotan <kamenokonokotan@gmail.com>
2022-08-25 02:02:16 +0900

    Update

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { downloadAndUnzipExtensions, downloadAndUnzipVSCode } from "./download";
 
 async function main() {
     await downloadAndUnzipVSCode("stable");
-    await downloadAndUnzipExtensions("siv3d-playground", "https://github.com/nokotan/siv3d-web-playground/releases/download/v0.0.15/siv3d-playground.tgz");
-    await downloadAndUnzipExtensions("emscripten-remote-build", "https://github.com/nokotan/emscripten-remote-build/releases/download/v0.0.2/emscripten-remote-build-npm-package");
+    await downloadAndUnzipExtensions("siv3d-playground", "https://github.com/nokotan/siv3d-web-playground/releases/download/v0.0.16/siv3d-playground.tgz");
+    await downloadAndUnzipExtensions("emscripten-remote-build", "https://github.com/nokotan/emscripten-remote-build/releases/download/v0.0.3/emscripten-remote-build.tgz");
     fs.copyFileSync("index.html", "vscode/vscode-web/index.html");
 }
 
