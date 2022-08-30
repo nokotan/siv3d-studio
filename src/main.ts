29a783faecb2e16fafc442670ddd5db7578607ce
nokotan <kamenokonokotan@gmail.com>
2022-08-31 03:00:01 +0900

    Update emscripten-remote-compile to v0.0.4

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { downloadAndUnzipExtensions, downloadAndUnzipVSCode } from "./download";
 async function main() {
     await downloadAndUnzipVSCode("stable");
     await downloadAndUnzipExtensions("siv3d-playground", "https://github.com/nokotan/siv3d-web-playground/releases/download/v0.0.17/siv3d-playground.tgz");
-    await downloadAndUnzipExtensions("emscripten-remote-build", "https://github.com/nokotan/emscripten-remote-build/releases/download/v0.0.3/emscripten-remote-build.tgz");
+    await downloadAndUnzipExtensions("emscripten-remote-build", "https://github.com/nokotan/emscripten-remote-build/releases/download/v0.0.4/emscripten-remote-build.tgz");
     fs.copyFileSync("index.html", "vscode/vscode-web/index.html");
 }
 
