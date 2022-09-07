87313f507a1104219c54f440fa31e76149776870
nokotan <nokotan@users.noreply.github.com>
2022-09-07 08:30:36 +0000

    deploy: e04c09c2216638abf0678b693f0fc26e70841171

---
+++ dev/out/vs/base/common/worker/simpleWorker.nls.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/define("vs/base/common/worker/simpleWorker.nls",{"vs/base/common/platform":["_"]});
+
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/784b0177c56c607789f9638da7b6bf3230d47a8c/core/vs/base/common/worker/simpleWorker.nls.js.map
---
+++ dev/out/vs/editor/common/services/editorSimpleWorker.nls.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/define("vs/editor/common/services/editorSimpleWorker.nls",{"vs/base/common/platform":["_"]});
+
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/784b0177c56c607789f9638da7b6bf3230d47a8c/core/vs/editor/common/services/editorSimpleWorker.nls.js.map
