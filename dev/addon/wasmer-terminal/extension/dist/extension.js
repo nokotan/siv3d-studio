b49e68baed8a42fb0b7e27c093734ecc7b1e9fc8
nokotan <nokotan@users.noreply.github.com>
2023-02-28 05:48:46 +0000

    deploy: f902db7bdb802e96a395bf6788bbe5d818d9473f

--- dev/addon/wasmer-terminal/extension/dist/extension.js
+++ dev/addon/wasmer-terminal/extension/dist/extension.js
@@ -1444,6 +1444,36 @@ module.exports = require("vscode");
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/create fake namespace object */
+/******/ 	(() => {
+/******/ 		var getProto = Object.getPrototypeOf ? (obj) => (Object.getPrototypeOf(obj)) : (obj) => (obj.__proto__);
+/******/ 		var leafPrototypes;
+/******/ 		// create a fake namespace object
+/******/ 		// mode & 1: value is a module id, require it
+/******/ 		// mode & 2: merge all properties of value into the ns
+/******/ 		// mode & 4: return value when already ns object
+/******/ 		// mode & 16: return value when it's Promise-like
+/******/ 		// mode & 8|1: behave like require
+/******/ 		__webpack_require__.t = function(value, mode) {
+/******/ 			if(mode & 1) value = this(value);
+/******/ 			if(mode & 8) return value;
+/******/ 			if(typeof value === 'object' && value) {
+/******/ 				if((mode & 4) && value.__esModule) return value;
+/******/ 				if((mode & 16) && typeof value.then === 'function') return value;
+/******/ 			}
+/******/ 			var ns = Object.create(null);
+/******/ 			__webpack_require__.r(ns);
+/******/ 			var def = {};
+/******/ 			leafPrototypes = leafPrototypes || [null, getProto({}), getProto([]), getProto(getProto)];
+/******/ 			for(var current = mode & 2 && value; typeof current == 'object' && !~leafPrototypes.indexOf(current); current = getProto(current)) {
+/******/ 				Object.getOwnPropertyNames(current).forEach((key) => (def[key] = () => (value[key])));
+/******/ 			}
+/******/ 			def['default'] = () => (value);
+/******/ 			__webpack_require__.d(ns, def);
+/******/ 			return ns;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
 /******/ 		// define getter functions for harmony exports
@@ -1602,7 +1632,9 @@ __webpack_require__.r(__webpack_exports__);
 
 async function activate(context) {
     __webpack_require__.p = context.extensionUri.toString() + "/dist/";
-    await (0,_pkg__WEBPACK_IMPORTED_MODULE_1__["default"])();
+    const wasmURL = await Promise.resolve(/* import() */).then(__webpack_require__.t.bind(__webpack_require__, 275, 17));
+    const wasmContent = await fetch(wasmURL.default);
+    await (0,_pkg__WEBPACK_IMPORTED_MODULE_1__["default"])(wasmContent);
     vscode__WEBPACK_IMPORTED_MODULE_0__.window.registerTerminalProfileProvider('wasmer-term.terminal', {
         provideTerminalProfile(token) {
             return new vscode__WEBPACK_IMPORTED_MODULE_0__.TerminalProfile({
